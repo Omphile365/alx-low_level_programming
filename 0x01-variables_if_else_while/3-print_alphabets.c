781a0f47e01ee648327cac6d74562f883da5e74d
Omphile365 <Omphiletau365@gmail.com>
2023-08-14 19:04:42 -0700

    print alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,11 +9,10 @@
 int main(void)
 {
 	char i;
-	char a;
 
-	for (i = 'a' ; <= 'z' ; i++)
+	for (i = 'a' ; i  <= 'z' ; i++)
 		putchar(i);
-	for (i = 'A' ; <= 'Z' ; i++)
+	for (i = 'A' ; i <= 'Z' ; i++)
 		putchar(i);
 	putchar('\n');
 	return (0);
