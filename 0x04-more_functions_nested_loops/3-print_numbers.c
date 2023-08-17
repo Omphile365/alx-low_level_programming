e5ef491540d458da84055f6351fdce7a8fe2a33c
Omphile365 <Omphiletau365@gmail.com>
2023-08-17 13:04:36 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *
+ * print_numbers - function that prints the numbers
+ *
+ * return: void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		_putchar(i + '0');
+	_putchar('\n');
+}
