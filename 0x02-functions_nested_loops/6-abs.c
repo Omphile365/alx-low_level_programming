e58eb99f581be3c862e1fe7048e742a0e5307fc8
Omphile365 <Omphiletau365@gmail.com>
2023-08-16 18:54:22 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - finds the absolute value of an integer
+ * @r: parameter should be checked
+ * Return: r or -r;
+ */
+
+int _abs(int)
+{
+	if (r < 0)
+		return (-r);
+	else if (r >= 0)
+	{
+		return (r);
+	}
+}
